eda7aeab43830bf1669bd09978bbafe3048a177e
--replace-all <jyan7794@uni.sydney.edu.au>
2020-02-18 14:30:07 +0800

    add .sql

---
+++ create.sql
@@ -0,0 +1,10 @@
+USE `p2pdatabase`;
+
+DROP TABLE IF EXISTS `peer`;
+
+CREATE TABLE `peer` (
+  `id` INT(10) NOT NULL AUTO_INCREMENT,
+  `temporaryId` CHAR(20) NOT NULL,
+  `name` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`)
+);
