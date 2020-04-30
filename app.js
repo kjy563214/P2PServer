2e66b25610162941d8c2d7062c85041920abbf7d
--replace-all <jyan7794@uni.sydney.edu.au>
2020-04-30 15:53:38 +1000

    to https

--- app.js
+++ app.js
@@ -2,24 +2,35 @@
  * Module dependencies.
  */
 var express = require('express')
-,	path = require('path');
-	
-var app = express();
+,	path = require('path')
+, https = require('https'
+, fs = require('fs'));
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
-
-var index = function(req, res) {
-    res.render('index', {
-      title : "WebRtcServerProject"
-    });
+var options = {
+    key: fs.readFileSync('./privatekey.pem'),
+    cert: fs.readFileSync('./certificate.pem')
 };
-app.get('/', index);
 
-var server = app.listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+var app = express();
+//
+// // all environments
+// app.set('port', process.env.PORT || 3000);
+// app.set('view engine', 'ejs');
+// app.use(express.static(path.join(__dirname, 'public')));
+//
+// var index = function(req, res) {
+//     res.render('index', {
+//       title : "WebRtcServerProject"
+//     });
+// };
+app.get('/', index);
+//
+// var server = app.listen(app.get('port'), function(){
+//   console.log('Express server listening on port ' + app.get('port'));
+// });
+
+var server = https.createServer(options, app).listen(3000, function () {
+    console.log('Https server listening on port ' + 3000);
 });
 
 var io = require('socket.io').listen(server);
